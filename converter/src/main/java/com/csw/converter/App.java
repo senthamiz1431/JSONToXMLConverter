3b72d1660b045358bbb07c88e0f1a7106c0e275f
senthamiz1431 <85439412+senthamiz1431@users.noreply.github.com>
2021-06-09 00:34:05 +0530

    Add files via upload

---
+++ converter/src/main/java/com/csw/converter/App.java
@@ -0,0 +1,40 @@
+package com.csw.converter;
+
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.csw.converter.converterfactory.ConverterFactory;
+import com.csw.converter.converterimpl.JSONXMLConverterI;
+
+public class App {
+
+	private static Logger logger = Logger.getLogger(App.class.getName());
+
+	public static void main(String[] args) {
+
+		JSONXMLConverterI jsonToXmlConverter = new ConverterFactory().getConverter();
+		try {
+			String jsonFilepath = args[0];
+			String xmlFilePath = args[1];
+
+			if (jsonFilepath != null && new File(jsonFilepath).exists()) {
+				if (xmlFilePath != null && new File(xmlFilePath).exists()) {
+					logger.log(Level.INFO, "JSONToXMLConverter starts \n JSON input : " + jsonFilepath
+							+ "\n XML output : " + xmlFilePath);
+
+					jsonToXmlConverter.convertJSONtoXML(jsonFilepath, xmlFilePath);
+
+					logger.log(Level.INFO, "JSONToXMLConverter ends");
+				} else {
+					logger.log(Level.SEVERE, "Invalid xml file path " + xmlFilePath);
+				}
+			} else {
+				logger.log(Level.SEVERE, "Invalid json file path " + jsonFilepath);
+			}
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "Error in converting JSON to XML : " + e.getMessage());
+		}
+
+	}
+}
